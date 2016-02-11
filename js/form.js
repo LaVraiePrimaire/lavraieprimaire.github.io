e1acfee5c0c2fa5ec2dba913f5ac96eb1c2afd5a
mikeyu152 <myu3@stanford.edu>
2016-02-10 21:41:40 -0800

    added play button and form

--- js/form.js
+++ js/form.js
@@ -8,14 +8,19 @@ $('#formname').submit(function() {
         type:'POST',
         success:function(data){
             var textbox = document.querySelector("#email");
-            textbox.style.display = "none";
+            textbox.value = "";
+            textbox.placeholder = "Merci!";
+            textbox.style.opacity = .2;
+            textbox.readOnly = true;
+            // textbox.style.display = "none";
             var button = document.querySelector("#submissionBtn");
-            button.style.display = "none";
-            var thanks = document.querySelector("#thanks");
-            thanks.style = button.style;
-            thanks.style.display = "inline";
+            // button.style.display = "none";
+            // var thanks = document.querySelector("#thanks");
+            // // thanks.style = button.style;
+            // thanks.style.display = "inline";
             // button.innerText = "Merci";
-            // button.disabled = true;
+            button.style.opacity = .2;
+            button.disabled = true;
             console.log("success!");
             // flash a message? 
         }
