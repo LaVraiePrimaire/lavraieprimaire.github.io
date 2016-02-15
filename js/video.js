60f2084fd79d2ac4593441153233a28d9128c436
Bogac Kerem Goksel <bkgoksel@gmail.com>
2016-02-15 17:34:43 +0200

    Day 1 responsive fixes
    Made the call to action/email panel responsive, fixed video autoplay,
    fixed new tab on links, fixed link content, rearranged partner logos to
    work responsively in IE and Firefox

---
+++ js/video.js
@@ -0,0 +1,29 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'pZwk5uAE51A',
+    modestbranding: 1,
+    origin: 'http://www.lavraieprimaire.fr',
+    rel: 0,
+    showinfo: 0,
+    autoplay: 0,
+    enablejsapi: 1,
+    hl: 'fr-fr',
+  });
+
+  $('#myModal').on('shown.bs.modal', function() {
+    player.playVideo();
+  });
+
+  $('#myModal').on('hidden.bs.modal', function() {
+    player.pauseVideo();
+  });
+}
